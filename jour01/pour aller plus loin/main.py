71d4e515ce8295694581f4021089b73ea5274a5c
guillaume-bomben <guillaume.bomben@laplateforme.io>
2023-11-20 12:05:39 +0100

    pour aller plus loin

--- jour01/pour aller plus loin/main.py
+++ jour01/pour aller plus loin/main.py
@@ -1,5 +1,11 @@
-test= "testee"
+test= "test"
+nbe = 0
 
 for lettre in test:
     if lettre == "e" :
-        print("Cette chaine de caractère contient la lettre e")
+        nbe += 1
+
+if nbe >= 1:
+    print("Cette chaine de caractère contient la lettre e")
+else:
+    print("Cette chaine de caractère ne contient la lettre e")
