949af37e1da8192fd5fa096ec1c0e57e9a286d64
guillaume-bomben <guillaume.bomben@laplateforme.io>
2023-11-21 12:50:06 +0100

    jour 2

---
+++ jour02/Pour aller plus loin/main.py
@@ -0,0 +1,24 @@
+a = int(input("entrez une longeur a : ")) 
+b = int(input("entrez une longeur b : ")) 
+c = int(input("entrez une longeur c : ")) 
+
+longeur = [a,b,c]
+print(sorted(longeur))
+part = 0
+
+if longeur[2] < longeur[0] + longeur[1]:
+    print("Ce triange est posible")
+    if longeur[2]**2 == longeur[0]**2+ longeur[1]**2:
+        print("Le Triange est rectangle")
+        part += 1
+    if longeur[0] == longeur[1] == longeur[2]:
+        print("Le Triangle est equilateral")
+        part += 1
+    elif longeur[0] == longeur[1] or longeur[1] == longeur[2] or longeur[2] == longeur[0]:
+        print("Le Triangle est isocele")
+        part += 1
+    if part == 0 :
+        print("Le Triangle est quelconque")
+
+else :
+    print("Ce triangle n'est pas possible")
