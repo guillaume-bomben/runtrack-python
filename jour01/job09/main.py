9d035dcead22171c24954eee5c0619fc577fe091
guillaume-bomben <guillaume.bomben@laplateforme.io>
2023-11-21 08:06:51 +0100

    f

--- jour01/job09/main.py
+++ jour01/job09/main.py
@@ -5,7 +5,8 @@ quantite=10
 print("Prix initial et stock de Basse :")
 print(f"{produit} : {prix} €/unité , {quantite} unité(s)")
 
-quantite += int(input("Nombre de produit a ajouter : \n"))
+quantite += 10
+quantite -= int(input("Nombre de produit a acheter : \n"))
 prix *= 1.1
 
 print("Prix et stock augmenter :")
