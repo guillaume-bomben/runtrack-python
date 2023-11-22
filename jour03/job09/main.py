98926fe28707ff3ad736b0f0ce3b5a3533b9d8ad
guillaume-bomben <guillaume.bomben@laplateforme.io>
2023-11-22 11:59:16 +0100

    jour 3

--- jour03/job09/main.py
+++ jour03/job09/main.py
@@ -1,16 +1,18 @@
 def moyenne(note1, note2, note3):
-    moyenne_etudiant = (note1 + note2 + note3)/3
-    if  15 <= moyenne_etudiant <=20:
-        print(f"Moyenne : {moyenne_etudiant} \n Tres bon élève")
-    elif  11 <= moyenne_etudiant <=14:
-        print(f"Moyenne : {moyenne_etudiant} \n Bon élève")
-    elif  8 <= moyenne_etudiant <=10:
-        print(f"Moyenne : {moyenne_etudiant} \n Elève moyen")
-    if  0 <= moyenne_etudiant <= 7:
-        print(f"Moyenne : {moyenne_etudiant} \n Elève devant faire des efforts")
+    return (note1 + note2 + note3)/3
+
 
 n1 = int(input("Entrez la première note :"))
 n2 = int(input("Entrez la seconde note :"))
 n3 = int(input("Entrez la troisième note :"))
-moyenne(n1,n2,n3)
+moyenne_etudiant = moyenne(n1,n2,n3)
+
+if  15 <= moyenne_etudiant <=20:
+    print(f"Moyenne : {moyenne_etudiant} \n Tres bon élève")
+elif  11 <= moyenne_etudiant <=14:
+    print(f"Moyenne : {moyenne_etudiant} \n Bon élève")
+elif  8 <= moyenne_etudiant <=10:
+    print(f"Moyenne : {moyenne_etudiant} \n Elève moyen")
+if  0 <= moyenne_etudiant <= 7:
+    print(f"Moyenne : {moyenne_etudiant} \n Elève devant faire des efforts")
 
